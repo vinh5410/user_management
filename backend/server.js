ed305f945ca7852c73586de8c0a5eb8d865a90ad
Vinhhc <vinhconghoang5410@gmail.com>
2025-12-19 18:06:30 +0700

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
